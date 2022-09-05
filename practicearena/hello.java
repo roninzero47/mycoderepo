5890d6b7acc71b3d35fab8f9ffb9d8dc3bb08a89
Avinash Padmanaban <avinashpadmanaban@protonmail.com>
2022-09-05 12:09:40 +0530

    learning about primitive and ref types

--- practicearena/hello.java
+++ practicearena/hello.java
@@ -1,8 +1,11 @@
-import java.util.Date;
-
+//import java.util.Date;
+import java.awt.*;
 class HelloWorld{
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
+        //Date now = new Date();
+        Point point1 = new Point(2,1);
+        Point point2 = point1;
+        point1.x = 4;
+        System.out.println(point2);
     }
 }
